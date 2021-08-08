5af1313249877f315872046dd94719ef32526503
RachmanForniandi <rachmanforniandi@gmail.com>
2021-08-08 21:09:43 +0700

    exception handling done

--- app/src/main/java/rachman/forniandi/workmanagerapp/data/work/RandomNumberWork.kt
+++ app/src/main/java/rachman/forniandi/workmanagerapp/data/work/RandomNumberWork.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import androidx.work.workDataOf
+import java.lang.Exception
 
 class RandomNumberWork(context: Context,workerParameters: WorkerParameters):
     Worker(context,workerParameters) {
@@ -12,27 +13,46 @@ class RandomNumberWork(context: Context,workerParameters: WorkerParameters):
     //doWork method runs asynchronously in background thread - this wont block our ui
     override fun doWork(): Result {
 
-        //get the input data
-        val start = inputData.getInt("KEY_START",0)
-        val count = inputData.getInt("KEY_COUNT",0)
+
         /*for (i in 0..count){
             Log.d(TAG,"RandomNumberWork: $i")
 
             Thread.sleep(1000)
         }*/
-        for (i in start..count){
-            Log.d(TAG,"RandomNumberWork: $i")
+        return try {
+            //get the input data
+            val start = inputData.getInt("KEY_START",0)
+            val count = inputData.getInt("KEY_COUNT",0)
+            for (i in start..count){
+                Log.d(TAG,"RandomNumberWork: $i")
 
-            Thread.sleep(1000)
+                if ( i == 5){
+                    throw Exception("Error on the for loop")
+                }
+                Thread.sleep(1000)
+            }
+            /*
+            pass data to result
+            workDataOf is a ktx extension method
+            */
+            val data = workDataOf("KEY_RESULT" to 10)
+
+            //return Result.success()
+            return Result.success(data)
+        }catch (e:Exception){
+
+            /*
+            * if there is an error return FAILURE
+            * */
+            //Result.failure()
+
+            /*
+            * you may want to retry it later then you can return retry
+            * */
+            Result.retry()
         }
-        /*
-        pass data to result
-        workDataOf is a ktx extension method
-        */
-        val data = workDataOf("KEY_RESULT" to 10)
 
-        //return Result.success()
-        return Result.success(data)
+
     }
 
     companion object{
--- app/src/main/java/rachman/forniandi/workmanagerapp/ui/home/HomeFragment.kt
+++ app/src/main/java/rachman/forniandi/workmanagerapp/ui/home/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -125,6 +126,10 @@ class HomeFragment : Fragment() {
 
         it.state == WorkInfo.State.CANCELLED->{
           homeViewModel.msg.value = "RandomNumberWork: CANCELLED"
+        }
+        it.state == WorkInfo.State.FAILED->{
+          homeViewModel.msg.value = "RandomNumberWork: FAILED :Error on the for loop"
+          Toast.makeText(requireActivity(),"Failed",Toast.LENGTH_LONG).show()
         }
         it.state.isFinished->{
           val resultOut =it.outputData.getInt("KEY_RESULT",0)

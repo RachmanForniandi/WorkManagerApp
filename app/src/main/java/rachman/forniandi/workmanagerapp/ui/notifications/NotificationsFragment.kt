c850517da00b6404f82db19f64aa38f8294b86cd
RachmanForniandi <rachmanforniandi@gmail.com>
2021-12-14 21:35:40 +0700

    execute coroutine worker done

--- app/src/main/java/rachman/forniandi/workmanagerapp/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/rachman/forniandi/workmanagerapp/ui/notifications/NotificationsFragment.kt
@@ -8,13 +8,18 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.work.*
 import rachman.forniandi.workmanagerapp.R
+import rachman.forniandi.workmanagerapp.data.work.EmployeeCoroutineWork
+import rachman.forniandi.workmanagerapp.data.work.RandomNumberPeriodicWork
 import rachman.forniandi.workmanagerapp.databinding.FragmentNotificationsBinding
+import java.util.concurrent.TimeUnit
 
 class NotificationsFragment : Fragment() {
 
   private lateinit var notificationsViewModel: NotificationsViewModel
-private var _binding: FragmentNotificationsBinding? = null
+  private var _binding: FragmentNotificationsBinding? = null
+  private lateinit var workManager: WorkManager
   // This property is only valid between onCreateView and
   // onDestroyView.
   private val binding get() = _binding!!
@@ -35,6 +40,30 @@ private var _binding: FragmentNotificationsBinding? = null
       textView.text = it
     })
     return root
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+
+    workManager = WorkManager.getInstance(requireContext())
+
+    //setup a periodic work request to insert data to local db
+    periodicWorkRequest()
+  }
+
+  private fun periodicWorkRequest(){
+    //constraints
+    val constraintsPeriodic = Constraints.Builder()
+      .setRequiredNetworkType(NetworkType.CONNECTED)
+      .build()
+
+    /*periodic work request*/
+    val randomNumberWorkRequest = PeriodicWorkRequest.Builder(
+      EmployeeCoroutineWork::class.java,15, TimeUnit.MINUTES)
+      .setConstraints(constraintsPeriodic)
+      .build()
+
+    workManager.enqueue(randomNumberWorkRequest)
   }
 
 override fun onDestroyView() {
